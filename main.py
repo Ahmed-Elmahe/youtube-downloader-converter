9ba6b6c33f79f69c429f1c682cc1ef9c51715689
Ahmed Al-Mahi <OfficialAlmahui@gmail.com>
2024-12-28 21:32:58 +0200

    � Adding in main file  while loop and another choice for exiting and change in the functions in youtube_dounloader file. in file_converter file change the package to moviepy

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 print("Welcome to NeuralNine YouTube Downloader and Converter v0.2 Alpha")
 print("Loading...")
 
-import pytube
+from pytube import YouTube, Playlist
 import youtube_downloader
 import file_converter
 
-print('''
-What do you want?
+while True:
+    print('''
+    What do you want?
 
-(1) Download YouTube Videos Manually
-(2) Download a YouTube Playlist
-(3) Download YouTube Videos and Convert Into MP3
+    (1) Download YouTube Videos Manually
+    (2) Download a YouTube Playlist
+    (3) Download YouTube Videos and Convert Into MP3
+    (4) Exit
 
-Downloading copyrighted YouTube videos is illegal!
-I am not responsible for your downloads! Go at your own risk!
+    Downloading copyrighted YouTube videos is illegal!
+    I am not responsible for your downloads! Go at your own risk!
 
-Copyright (c) NeuralNine 2020
-''')
+    Copyright (c) NeuralNine 2020
+    ''')
 
-choice = input("Choice: ")
+    choice = input("Choice: ")
 
-if choice == "1" or choice == "2":
-    quality = input("Please choose a quality (low, medium, high, very high):")
-    if choice == "2":
-        link = input("Enter the link to the playlist: ")
-        print("Downloading playlist...")
-        youtube_downloader.download_playlist(link, quality)
-        print("Download finished!")
-    if choice == "1":
+    if choice == "1" or choice == "2":
+        quality = input("Please choose a quality (low, medium, high, very high):")
+        if choice == "2":
+            link = input("Enter the link to the playlist: ")
+            print("Downloading playlist...")
+            youtube_downloader.download_playlist(link, quality)
+            print("Download finished!")
+        if choice == "1":
+            links = youtube_downloader.input_links()
+            for link in links:
+                youtube_downloader.download_video(link, quality)
+    elif choice == "3":
         links = youtube_downloader.input_links()
         for link in links:
-            youtube_downloader.download_video(link, quality)
-elif choice == "3":
-    links = youtube_downloader.input_links()
-    for link in links:
-        print("Downloading...")
-        filename = youtube_downloader.download_video(link, 'low')
-        print("Converting...")
-        file_converter.convert_to_mp3(filename)
-else:
-    print("Invalid input! Terminating...")
+            print("Downloading...")
+            filename = youtube_downloader.download_video(link, 'low')
+            print("Converting...")
+            file_converter.convert_to_mp3(filename)
+    elif choice == "4":
+        break
+    else:
+        print("Invalid input! Terminating...")
+
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,33 +1,45 @@
-import pytube
+from pytube import YouTube, Playlist
+from urllib.error import HTTPError
 
+"""
+Handle HTTP 403 Errors: YouTube might block requests for certain formats or resolutions.You could try different itag values or catch
+the error and retry with a different resolution
+"""
 def download_video(url, resolution):
-    itag = choose_resolution(resolution)
-    video = pytube.YouTube(url)
-    stream = video.streams.get_by_itag(itag)
-    stream.download()
-    return stream.default_filename
+    try:
+        video = YouTube(url)
+        stream = get_stream_by_resolution(video, resolution)
+        if stream:
+            stream.download()
+            return stream.default_filename
+        else:
+            print(f"Stream not available for resolution {resolution}. Trying lower resolution.")
+            return download_video(url, "low")
+    except HTTPError as e:
+        print(f"HTTP error occurred: {e}")
+        return None
 
 def download_videos(urls, resolution):
     for url in urls:
         download_video(url, resolution)
 
 def download_playlist(url, resolution):
-    playlist = pytube.Playlist(url)
+    playlist = Playlist(url)
     download_videos(playlist.video_urls, resolution)
 
-def choose_resolution(resolution):
+def get_stream_by_resolution(video, resolution):
     if resolution in ["low", "360", "360p"]:
-        itag = 18
+        stream = video.streams.filter(res="360p", file_extension="mp4").first()
     elif resolution in ["medium", "720", "720p", "hd"]:
-        itag = 22
+        stream = video.streams.filter(res="720p", file_extension="mp4").first()
     elif resolution in ["high", "1080", "1080p", "fullhd", "full_hd", "full hd"]:
-        itag = 137
+        stream = video.streams.filter(res="1080p", file_extension="mp4").first()
     elif resolution in ["very high", "2160", "2160p", "4K", "4k"]:
-        itag = 313
+        stream = video.streams.filter(res="2160p", file_extension="mp4").first()
     else:
-        itag = 18
-    return itag
+        stream = video.streams.filter(res="360p", file_extension="mp4").first()
 
+    return stream
 
 def input_links():
     print("Enter the links of the videos (end by entering 'STOP'):")
@@ -35,10 +47,9 @@ def input_links():
     links = []
     link = ""
 
-    while link != "STOP" and link != "stop":
+    while link.lower() != "stop":
         link = input()
-        links.append(link)
-
-    links.pop()
+        if link.lower() != "stop":
+            links.append(link)
 
     return links
